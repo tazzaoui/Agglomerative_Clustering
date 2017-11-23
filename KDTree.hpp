088125a0329e02652c805d1e660cf0ab841aad20
Taha Azzaoui <tazzaoui@cs.uml.edu>
2017-11-23 15:32:19 -0500

    Added more statics for node manipulation

--- KDTree.hpp
+++ KDTree.hpp
@@ -16,15 +16,22 @@ private:
   size_t size;
   size_t dim;
   Node* root;
-  /*Helper functions for Node manipulation*/
-  static bool is_same(Node& a, Node& b, size_t dim);
+  /*Helper statics for Node manipulation*/
+  static bool same_points(double* a, double* b, size_t dim);
   static Node* get_node(double* p, size_t dim);
   static void insert_node(Node** root, double* point,
 			   size_t dim, size_t depth = 0);
+  static bool find_node(Node* root, double* p, size_t dim, size_t depth = 0);
+  static void output_in_order(Node*, size_t);
+  static void print_point(double* p, size_t dim);
+  static void clear_tree(Node** root);
 public:
   KDTree(size_t dim);
+  ~KDTree();
   bool insert(double* point, size_t depth = 0);
-  Node find(double* p, size_t depth = 0);
+  bool contains(double* p, size_t depth = 0);
+  void print(void);
 };
+
 
 #endif
