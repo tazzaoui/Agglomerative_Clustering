b83288212f34905e603dfae210c1c82e62b76eae
Taha Azzaoui <tazzaoui@cs.uml.edu>
2017-11-23 22:13:59 -0500

    Updated prototypes

--- KDTree.hpp
+++ KDTree.hpp
@@ -15,27 +15,34 @@ struct node{
 class KDTree{
   
 private:
-  size_t size;
+  size_t num_elements;
   size_t dim;
   Node* root;
   /*Helper statics for Node manipulation*/
   static bool same_points(double* a, double* b, size_t dim);
   static Node* get_node(double* p, size_t dim);
-  static void insert_node(Node** root, double* point,
+  static Node* insert_node(Node* root, double* point,
 			   size_t dim, size_t depth = 0);
-  static bool find_node(Node* root, double* p, size_t dim, size_t depth = 0);
+  static Node* min_node(Node*, Node*, Node*, size_t);
+  static Node* delete_node(Node* root, double* point, size_t dim, size_t depth = 0);
+  static Node* find_min(Node* root, size_t dim, size_t qdim, size_t depth = 0);
+  static void copy_points(double* src, double* dest, size_t dim);
   static void output_in_order(Node*, size_t);
+  static Node* find_node(Node* root, double* p, size_t dim, size_t depth = 0);
   static void print_point(double* p, size_t dim);
-  static void clear_tree(Node** root);
-  static double locate_min(Node* root, size_t dim,size_t qdim, size_t depth = 0);
+  static void clear_tree(Node* root);
+  static bool  contains_point(Node* root, double* point, size_t dim, size_t depth = 0);
+  
 public:
   KDTree(size_t dim);
   ~KDTree();
-  bool insert(double* point, size_t depth = 0);
-  bool contains(double* p, size_t depth = 0);
-  double find_min(size_t dim, size_t depth=0);
+  bool insert(double* point);
+  bool remove(double* point, size_t depth = 0);
+  bool contains(double* p);
+  double get_min(size_t dim, size_t depth = 0);
   void print(void);
+  size_t size()const;
 };
 
-
 #endif
+
