822670eb2a301ed06e78649964aaaad983425017
Taha Azzaoui <tazzaoui@cs.uml.edu>
2017-11-28 01:22:33 -0500

    Removed Extraneous Cluster Functions

--- KDTree.hpp
+++ KDTree.hpp
@@ -5,14 +5,11 @@
 #include <limits.h>
 #include <algorithm>
 #include <vector>
-
-typedef struct cluster{
-  std::vector<double*> data; /* Data points */
-  double* point; /* Classificatoin point e.g. centroid */
-}Cluster;
+#include "Cluster.hpp"
 
 typedef struct node Node;
 struct node{
+  node(Cluster &c);
   Cluster c;
   Node* right;
   Node* left;
@@ -26,33 +23,26 @@ private:
   size_t dim;
   Node* root;
   /*Helper statics for Node manipulation*/
-  static bool same_points(double* a, double* b, size_t dim);
-  static bool same_data(std::vector<double*>& a, std::vector<double*>& b, size_t dim);
-  static Node* get_node(Cluster cluster, size_t dim);
-  static Node* insert_node(Node* root, Cluster c,
+  static Node* get_node(Cluster &c);
+  static Node* insert_node(Node* root, Cluster &c,
 			   size_t dim, size_t depth = 0); 
   static Node* min_node(Node*, Node*, Node*, size_t);
   static Node* delete_node(Node* root, Cluster c, size_t dim, size_t depth = 0);
   static Node* find_min(Node* root, size_t dim, size_t qdim, size_t depth = 0);
-  static void copy_points(double* src, double* dest, size_t dim);
   static void output_in_order(Node*, size_t);
   static Node* find_node(Node* root, Cluster c, size_t dim, size_t depth = 0);
-  static void print_point(double* p, size_t dim);
-  static void print_cluster(Cluster c, size_t dim);
   static void clear_tree(Node* root);
-  static bool contains_point(Node* root, double* point, size_t dim, size_t depth = 0);
-  static bool contains_cluster(Node* root, Cluster c, size_t dim, size_t depth = 0); 
-  static bool same_clusters(Cluster &a, Cluster& b, size_t dim);
-  static void copy_clusters(Cluster &src, Cluster &dest, size_t dim);
+  static bool contains_cluster(Node* root, Cluster c, size_t dim, size_t depth=0);
+  static bool contains_point(Node* root, double* point, size_t dim, size_t depth);
   static Cluster nearest(Node* root, Cluster* c, Cluster* min, size_t dim, size_t depth=0);
   static Cluster best_match(Node* root, Cluster* c, Cluster* min, size_t dim, size_t depth=0);
-  static double euclidian_distance(double* p1, double* p2, size_t dim);
+  static Cluster get_first_not_eq(Node* root, Cluster& c);
 public:
   KDTree(size_t dim);
   ~KDTree();
-  bool insert(Cluster c); 
-  bool remove(Cluster c); 
-  bool contains(Cluster c); 
+  bool insert(const Cluster c); 
+  bool remove(const Cluster c); 
+  bool contains(const Cluster c); 
   Cluster get_min(size_t dim);
   Cluster nearest_neighbor(Cluster c);
   Cluster find_best_match(Cluster c);
