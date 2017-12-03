58fbc8f765c8d23d03dc9397ae540baa98af59e5
Taha Azzaoui <tazzaoui@cs.uml.edu>
2017-12-03 13:08:12 -0500

    Const correct header

--- Cluster.hpp
+++ Cluster.hpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Cluster{
 private:
-  std::vector<double*>* data; 
+  vector<double*>* data; 
   double* point; 
   size_t dim;
   void get_centroid(double* centroid, std::vector<double*> &v, size_t dim);
@@ -17,13 +17,14 @@ public:
   Cluster(std::vector<double*>& data, size_t dim);
   Cluster(const Cluster&, const Cluster&);
   Cluster(const Cluster&);
+  
   const double* get_point(void)const;
   const vector<double*>* get_data(void)const;
   size_t get_dim() const;
   Cluster& operator=(const Cluster& rhs);
-  bool operator==(const Cluster& rhs);
+  bool operator==(const Cluster& rhs)const;
   friend ostream& operator<<(ostream&, const Cluster&);
-  double euclidian_distance(Cluster&);
+  double euclidian_distance(Cluster const&) const;
   ~Cluster();
 };
 #endif
